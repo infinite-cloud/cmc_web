0ac6385d11931d285af37cec7c421d5ec7bbf0a8
infinite-cloud <54478207+infinite-cloud@users.noreply.github.com>
2020-02-23 10:50:25 +0300

    Add scripts for database deletion

---
+++ db/drop-db.sql
@@ -0,0 +1,3 @@
+-- Drop the entire database --
+
+DROP DATABASE IF EXISTS bookstore;
---
+++ db/drop-tables.sql
@@ -0,0 +1,10 @@
+-- Drop every table in the database, leaving the database intact --
+
+DROP TABLE IF EXISTS ordered_book;
+DROP TABLE IF EXISTS purchase;
+DROP TABLE IF EXISTS book_author;
+DROP TABLE IF EXISTS book;
+DROP TABLE IF EXISTS publisher;
+DROP TABLE IF EXISTS genre;
+DROP TABLE IF EXISTS cover_type;
+DROP TABLE IF EXISTS account;
