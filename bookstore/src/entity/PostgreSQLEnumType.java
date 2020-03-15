6a32dead6543bf4873dc5165ef723940145c4832
infinite-cloud <54478207+infinite-cloud@users.noreply.github.com>
2020-03-15 10:58:11 +0300

    Import org.hibernate.type.EnumType

--- bookstore/src/entity/PostgreSQLEnumType.java
+++ bookstore/src/entity/PostgreSQLEnumType.java
@@ -2,12 +2,13 @@ package entity;
 
 import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.type.EnumType;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Types;
 
-public class PostgreSQLEnumType extends org.hibernate.type.EnumType {
+public class PostgreSQLEnumType extends EnumType {
     public void nullSafeSet(PreparedStatement st, Object value,
             int index, SharedSessionContractImplementor session)
             throws HibernateException, SQLException {
