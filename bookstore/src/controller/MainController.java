62e414a10eaaeeaf38da30ef9a400a70b04eb5cd
infinite-cloud <54478207+infinite-cloud@users.noreply.github.com>
2020-04-05 18:25:13 +0300

    Implement a basic index.jsp

--- bookstore/src/controller/MainController.java
+++ bookstore/src/controller/MainController.java
@@ -1,17 +1,47 @@
 package controller;
 
+import daoimpl.BookAuthorDAOImpl;
+import daoimpl.BookDAOImpl;
+import entity.AuthorEntity;
+import entity.BookAuthorEntity;
+import entity.BookEntity;
+import javafx.util.Pair;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @Transactional
 @EnableWebMvc
 public class MainController {
+    @Autowired
+    private BookDAOImpl bookDAO;
+    @Autowired
+    private BookAuthorDAOImpl bookAuthorDAO;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home() {
+    public String home(ModelMap modelMap) {
+        bookDAO.setSession();
+        bookAuthorDAO.setSession();
+
+        List<BookEntity> books = bookDAO.getAll();
+        Map<BookEntity, List<AuthorEntity>> result = new HashMap<>();
+
+        for (BookEntity book : books) {
+            result.put(book, bookAuthorDAO.getAuthorsByBook(book));
+        }
+
+        modelMap.addAttribute("bookList", result);
+
         return "index";
     }
 }
--- bookstore/src/webapp/pages/index.jsp
+++ bookstore/src/webapp/pages/index.jsp
@@ -1,15 +1,43 @@
-﻿<!DOCTYPE html>
+﻿<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset = "UTF-8">
     <title>Книжный магазин</title>
-    <link rel = "stylesheet" type = "text/css" href = "${pageContext.request.contextPath}/resources/css/styles.css">
+    <link rel = "stylesheet" type = "text/css"
+          href = "${pageContext.request.contextPath}/resources/css/styles.css">
 </head>
 <body>
 
 <jsp:include page = "header.jsp"/>
 
-<div class = "page-title">Каталог</div>
+<table border = "1" style = "width:100%">
+    <tr>
+        <th>Название</th>
+        <th>Авторы</th>
+        <th>Цена</th>
+        <th>Наличие</th>
+    </tr>
+    <c:forEach items = "${bookList}" var = "book">
+        <tr>
+            <td>${book.key.bookName}</td>
+            <td>
+                <c:forEach items = "${book.value}" var = "author">
+                    ${author.authorName}<br/>
+                </c:forEach>
+            </td>
+            <td>${book.key.bookPrice} руб.</td>
+            <td>
+                <c:if test = "${book.key.availableCount > 0}">
+                    В наличии
+                </c:if>
+                <c:if test = "${book.key.availableCount == 0}">
+                    Нет в наличии
+                </c:if>
+            </td>
+        </tr>
+    </c:forEach>
+</table>
 
 <jsp:include page = "footer.jsp"/>
 
